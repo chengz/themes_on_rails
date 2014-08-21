64d045f57ef3c2e1c3e3b68204d4d723d6a65032
Cheng Zhu <cheng@weteachme.com>
2014-08-21 13:11:36 +1000

    do not use asset pipeline

--- lib/themes_on_rails/railtie.rb
+++ lib/themes_on_rails/railtie.rb
@@ -7,19 +7,19 @@ module ThemesOnRails
       end
     end
 
-    initializer "themes_on_rails.assets_path" do |app|
-      Dir.glob("#{Rails.root}/app/themes/*/assets/*").each do |dir|
-        app.config.assets.paths << dir
-      end
-    end
+    #initializer "themes_on_rails.assets_path" do |app|
+    #  Dir.glob("#{Rails.root}/app/themes/*/assets/*").each do |dir|
+    #    app.config.assets.paths << dir
+    #  end
+    #end
 
-    if !Rails.env.development? && !Rails.env.test?
-      initializer "themes_on_rails.precompile" do |app|
-        app.config.assets.precompile += [ Proc.new { |path, fn| fn =~ /app\/themes/ && !%w(.js .css).include?(File.extname(path)) } ]
-        app.config.assets.precompile += Dir["app/themes/*"].map { |path| "#{path.split('/').last}/all.js" }
-        app.config.assets.precompile += Dir["app/themes/*"].map { |path| "#{path.split('/').last}/all.css" }
-      end
-    end
+    #if !Rails.env.development? && !Rails.env.test?
+    #  initializer "themes_on_rails.precompile" do |app|
+    #    app.config.assets.precompile += [ Proc.new { |path, fn| fn =~ /app\/themes/ && !%w(.js .css).include?(File.extname(path)) } ]
+    #    app.config.assets.precompile += Dir["app/themes/*"].map { |path| "#{path.split('/').last}/all.js" }
+    #    app.config.assets.precompile += Dir["app/themes/*"].map { |path| "#{path.split('/').last}/all.css" }
+    #  end
+    #end
 
   end
 end
